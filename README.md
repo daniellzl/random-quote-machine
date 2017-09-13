4f7a017cfc0f7687aca8d8ba2e7f9f6960b03be5
daniellzl <mr.daniel.liang@gmail.com>
2017-09-13 14:57:20 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Random Quote Machine
+
+Displays random quotes with the click of a button.
+
+[Live Application](random-quote-machine-dlzl.surge.sh)
+
+### Project Goals
+
+Application was completed as a freeCodeCamp [challenge](https://www.freecodecamp.org/challenges/build-a-random-quote-machine).
+
+1. User Story: I can click a button to show me a new random quote.
+
+2. User Story: I can press a button to tweet out a quote.
+
+### Technologies
+
+* font awesome
+* forismatic api
+* jquery
---
+++ scripts.js
@@ -0,0 +1,31 @@
+var quote = document.querySelector('.quote');
+var author = document.querySelector('.author');
+var newQuote = document.querySelector('.new-quote');
+var twitter = document.querySelector('.twitter');
+var quoteMachine = $('.quote-machine');
+var body = document.querySelector('body');
+var colors = ["#cbbeb5", "#ff6666", "#525266", "#423f3b", "#ffe5a9", "#dbcccc", "#8faab3", "#c4bd8b", "#81b14f", "#451b04"];
+
+function newIndex() {
+    return Math.floor(Math.random() * colors.length);
+}
+
+function addNewQuote() {
+    quoteMachine.fadeOut(1000, function() {
+        $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function(json) {
+            quote.textContent = json.quoteText;
+            if (json.quoteAuthor === '') {
+                json.quoteAuthor = 'Anonymous';
+            }
+            author.textContent = '- ' + json.quoteAuthor;
+            twitter.setAttribute('href', "https://twitter.com/intent/tweet?text=" + json.quoteText + "~" + json.quoteAuthor);
+        });
+        var index = newIndex();
+        body.style.background = colors[index];
+        newQuote.style.background = colors[index];
+        quoteMachine.fadeIn(1000);
+    });
+}
+
+addNewQuote();
+newQuote.addEventListener('click', addNewQuote);
